5a7be88e593ab19525e75f15de26ace26e2190ed
schuprinenko <skillova@yandex.ru>
2024-02-28 07:24:34 +0000

    in class Category: set atr:products - private +add decorator @property <getter> / <setter> for atr:self.__products; +add decorator @property <getter>. in Classes folder: +add file __init__.py.

--- src/Classes/category.py
+++ src/Classes/category.py
@@ -7,6 +7,32 @@ class Category:
     def __init__(self, name: str, description: str, products: list = None) -> None:
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
         Category.instance_counter += 1
-        Category.product_counter += len(self.products)
+        Category.product_counter += len(self.__products)
+
+    @property
+    def set_products(self):
+        """Геттер приватного атрибута класса <self.__products>"""
+        return self.__products
+
+    @set_products.setter
+    def set_products(self, new_product):
+        """Сеттер приватного атрибута класса <self.__products>"""
+        for product in self.__products:
+            if product.name == new_product.name:
+                product.quantity += new_product.quantity
+                if new_product.price >= 0:
+                    product.price = new_product.price
+                    return
+            else:
+                self.__products.append(new_product)
+                return
+
+    @property
+    def get_format_products(self):
+        """Возвращает список продуктов в формате (<name>, <price> руб. Остаток: <quantity> шт.)"""
+        product_list = []
+        for product in self.__products:
+            product_list.append(f'{product.name}, {product.price} руб. Остаток: {product.quantity} шт.')
+        return product_list
