0a2d495b7d56ab8c00ca286eb1733d3e64b02fb7
schuprinenko <skillova@yandex.ru>
2024-03-02 17:22:38 +0000

    clear iso code

--- src/Classes/smartphone.py
+++ src/Classes/smartphone.py
@@ -6,7 +6,6 @@ class SmartPhone(Product):
     def __init__(self, name: str, model: str, efficiency: str, memory: str, color: str, description: str,
                  price: float, quantity: int):
         super().__init__(name, description, color, price, quantity)
-        self.name = name
         self.efficiency = efficiency
         self.model = model
         self.memory = memory
